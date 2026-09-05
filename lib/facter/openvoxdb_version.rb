25087451f9a25023a7238fcf3eef2c73b19358e1
Corporate Gadfly <haroon.rafique@gmail.com>
2026-09-05 16:09:39 -0400

    replace deprecated Facter::Util::Resolution.which
    Signed-off-by: Corporate Gadfly <haroon.rafique@gmail.com>

--- lib/facter/openvoxdb_version.rb
+++ lib/facter/openvoxdb_version.rb
@@ -1,5 +1,5 @@
 Facter.add(:openvoxdb_version) do
-  confine { Facter::Util::Resolution.which('puppetdb') }
+  confine { Facter::Core::Execution.which('puppetdb') }
 
   setcode do
     output = Facter::Core::Execution.execute('puppetdb --version')
